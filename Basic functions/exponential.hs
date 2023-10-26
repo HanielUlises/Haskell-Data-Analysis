03d837cc683cf3375eebedfa41466bf28884cc7a
Haniel Ulises <hanielulises2003@gmail.com>
2023-10-25 23:21:04 -0600

    Fast exponentiation

--- Basic functions/exponential.hs
+++ Basic functions/exponential.hs
@@ -1,10 +1,9 @@
 fastExponent :: Integer -> Integer -> Integer 
 
 fastExponent _ 0 = 1
-fastExponent x n = 
-    let y = fastExponent x n_halved
+fastExponent x n 
+    | even n = y * y
+    | otherwise = y * y * x
+    where 
+        y = fastExponent x n_halved
         n_halved = div n 2
-    in 
-        if even n 
-        then y * y
-        else y * * x
